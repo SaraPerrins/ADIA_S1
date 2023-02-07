e5ce607b339f40e4edf65b135268ed49c0837008
ellenvermes <107133651+ellenvermes@users.noreply.github.com>
2023-02-07 15:12:01 -0500

    added table statements for regression sample size

--- analyses/depress/2.inference_depress.r
+++ analyses/depress/2.inference_depress.r
@@ -55,7 +55,7 @@ pred.cls <- predict(fit.cls,
 
 cbind(unique(dat$node.cls), pred.cls, confint(pred.cls))[order(unique(dat$node.cls)),]
 #to find sample size
-table(df1$node.cls)
+table(df1$node.cls[!is.na(df1$y)])
 #with(df1,ftable(ACEmentalill,female,ACEphysharm,node.cls))
 
 #======================================================================================
@@ -109,7 +109,9 @@ pred.cnd <- predict(fit.cnd,
                     )
                     
 cbind(unique(dat$node.cnd), pred.cnd, confint(pred.cnd))[order(unique(dat$node.cnd)),]
-table(df1$node.cnd)
+#to find sample size
+#those with missing covariates are going to be dropped
+table(df1$node.cnd[complete.cases(df1[,all.vars(formula(fit.cnd))])])
 #======================================================================================
 #3.- Causal tree
 #======================================================================================
@@ -135,4 +137,5 @@ cbind(unique(dat$node.cau),
   coef(fit1)[grep("anyACE_T:", names(coef(fit1)))],
   confint(fit1)[grep("anyACE_T:", names(coef(fit1))), ]
   )[order(unique(dat$node.cau)), ]
-table(df1$node.cau)
+#to find sample size
+table(df1$node.cau[complete.cases(df1[,all.vars(formula(fit1))])])
