bff6096c7f9bfa305e03029b86f37e349e424dcc
ellenvermes <107133651+ellenvermes@users.noreply.github.com>
2023-05-25 09:34:18 -0400

    updated levels statement

--- analyses/preshlth/3.additional tests_preshlth.R
+++ analyses/preshlth/3.additional tests_preshlth.R
@@ -21,8 +21,6 @@ dim(dat)
 dat <-  dat %>% filter(training.sample == 0)
 dim(dat)
 
-summary(dat$w)
-
 #========================================================================
 # causal present health tree
 #========================================================================
@@ -42,9 +40,11 @@ dat <-
              , anyACE_T == 0 &  (bneedin == 1) ~ "OCS"
              , anyACE_T == 0 & !(bneedin == 1) ~ "None"
            )
-         , ace_ocs = factor(ace_ocs)
-         , ace_ocs = relevel(ace_ocs, ref = "None")
-  )
+   , ace_ocs = factor(ace_ocs
+  , levels = c("None", "ACE", "ACE + OCS", "OCS")
+  ))
+
+levels(dat$ace_ocs)
 
 table(dat$ace_ocs, useNA = "ifany")
 
@@ -59,11 +59,10 @@ source("R/ebw.r")
 # the following code can be skipped if no modifications are requiered
 # covariates/confounding
 z <- c("female", "agegrp", "white", "hisp", "black", "asian","asian_nhpi", "othrace", "mhighgd_bin"
-       , "rural", "mixur"
+       ,"rural", "mixur"
        #,
        #"mhhinco" removing income as covariate
        )
-
 
 # Ye: I replaced income with income adjusted in preprocess 01/18/2023
 dat$Z <- dat[, z]
@@ -110,8 +109,7 @@ grp <- c("None", "ACE", "ACE + OCS", "OCS")
 weights <-
   lapply(grp, \(g) {
     with(dat[!is.na(dat$ace_ocs) & dat$ace_ocs == g, ],
-      find_weights(id = id
-      , covariates = C, target.margins = tgt, base.weight = w)
+         ebw(id = id, covariates = C, target.margins = tgt, base.weight = w)
     )
   }) %>%
   bind_rows
@@ -119,9 +117,6 @@ weights <-
 colnames(weights)[2] <- "new_w"
 dat  <- left_join(dat, weights, by = "id")
 dat$new_w[is.na(dat$new_w)] <- 0
-
-with(dat, plot(sqrt(w/sum(w)), sqrt(new_w)))
-with(dat[dat$w>0, ], cor(sqrt(w/sum(w)), sqrt(new_w)))
 
 
 tb_r <-
