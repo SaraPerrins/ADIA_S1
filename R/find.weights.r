df15d76d5bf97d29bdc4899a2e792d86a05c0445
ellenvermes <107133651+ellenvermes@users.noreply.github.com>
2023-08-28 15:28:49 -0400

    Most up to date

--- R/find.weights.r
+++ R/find.weights.r
@@ -1,38 +1,23 @@
 library(CVXR)
 
-my_scale <- function(covariates, target_margins) {
-  sigma <- sqrt(colMeans(covariates * covariates) - colMeans(covariates)^2)
-  sigma[colMeans(covariates) < .05] <- sqrt(.05 * .95)
-  sigma[colMeans(covariates) > .95] <- sqrt(.05 * .95)
-  scale(covariates, center = target_margins, scale = sigma)
-}
-
-
-find_weights <- function(id, covariates
-                         , lambda = 0.1
-                         #' initial set of weights, sampling weights
-                         , base.weights   = rep(1 / n, n)
-                         , target.margins = colMeans(X)
-                         ) {
+find.weights <- function(phi_X, lambda=0) {
   #https://web.stanford.edu/~boyd/papers/pdf/cvxr_paper.pdf
   #  see page 18  Kelly gambling
-  n     <-  nrow(covariates)
-  w     <-  Variable(n)
-  x     <-  my_scale(covariates, target.margins)
-
-  objtive       <- Minimize(
-      sum((t(x) %*% w)^2) +
-      lambda * (w * log(w / base.weights)) # Kullback (1959) entropy divergence
-      )
-
-  constraints   <- list(w >= 0, sum(w) == 1)
-
-  problem       <- Problem(objtive, constraints = constraints)
-
+  n             <-  dim(phi_X)[1]
+  
+  w             <- Variable(n)
+  
+  objtive       <- Minimize(sum((t(phi_X)%*%w )^2) + lambda*n*sum(w^2))
+  
+  constraints   <- list(w >= 0,sum(w)==1)
+  
+  problem       <- Problem(objtive,constraints = constraints)
+  
   result        <- solve(problem)
-
+  
   print(result$status)
-  slw <- round(result$getValue(w), 4)
+  slw <- round(result$getValue(w),4)
   return(slw)
-  return(data.frame(id=id,wb=ebal$w))
 }
+##test editing Ye
+#the test worked!!!
